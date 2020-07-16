d28eb8a48da15479250076ee883ea3f4a7f18726
段男神 <782283977@qq.com>
2020-07-16 15:38:25 +0800

    版本1.1：20.7.16 15.38

--- day4/src/com/neusoft/demo/Test1.java
+++ day4/src/com/neusoft/demo/Test1.java
@@ -3,5 +3,6 @@ package com.neusoft.demo;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("1");
+        System.out.println("2020.7.16-15.37");
     }
 }
